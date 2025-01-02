7c9f535717560bf4802e959eced8464620081749
woogie01 <kwonhu3973@gmail.com>
2025-01-02 20:08:33 +0900

    [Feat] : Security 설정 추가

---
+++ src/main/java/com/tools/seoultech/timoproject/member/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.tools.seoultech.timoproject.member.config;
+
+import com.tools.seoultech.timoproject.member.service.OAuth2MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2MemberService oAuth2MemberService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .loginPage("/login")
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
+                                userInfoEndpointConfig.userService(oAuth2MemberService)));
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(oAuth2MemberService)));
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        //.requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        //.anyRequest().authenticated());
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+}
