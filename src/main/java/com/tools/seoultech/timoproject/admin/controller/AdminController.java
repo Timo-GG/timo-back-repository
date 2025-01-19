1163ce3e0254a068c89da822c2e048e13e4527a2
changha <mentenseoul@gmail.com>
2025-01-19 20:36:45 +0900

    [Feat] 어드민 로그인 관련
    어드민 로그인 관련 파트 입니다.

--- src/main/java/com/tools/seoultech/timoproject/admin/controller/AdminController.java
+++ src/main/java/com/tools/seoultech/timoproject/admin/controller/AdminController.java
@@ -1,7 +1,10 @@
-package com.tools.seoultech.timoproject.admin;
+package com.tools.seoultech.timoproject.admin.controller;
 
+import com.tools.seoultech.timoproject.admin.LoginRequired;
 import com.tools.seoultech.timoproject.admin.service.AdminService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,24 +14,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
+@Slf4j
 public class AdminController {
 
     private final AdminService adminService;
 
+    @LoginRequired
     @GetMapping("")
-    public String index() {
+    public String index(HttpSession session) {
+        log.info("session: {}", session.getAttribute("isAdmin"));
+        if (session.getAttribute("isAdmin") == null) {
+            return "redirect:/admin/login";
+        }
         return "dashboard/index"; // main page
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/admin/login";
+    }
+
     @GetMapping("/login")
-    public String adminLogin() {
+    public String adminLogin(@RequestParam(required = false) String error, org.springframework.ui.Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Invalid username or password");
+        }
         return "login"; // login.html 렌더링
     }
 
     @PostMapping("/login")
-    public String processLogin(@RequestParam String username, @RequestParam String password) {
+    public String processLogin(@RequestParam String username, @RequestParam String password, HttpSession session) {
         // AdminService를 사용하여 인증 처리
         if (adminService.authenticate(username, password)) {
+            // 세션에 ADMIN 정보 저장
+            session.setAttribute("isAdmin", true);
             return "redirect:/admin"; // 로그인 성공 시 대시보드로 이동
         } else {
             return "redirect:/admin/login?error=true"; // 로그인 실패 시 로그인 페이지로 리다이렉트
--- src/main/java/com/tools/seoultech/timoproject/admin/service/AdminService.java
+++ src/main/java/com/tools/seoultech/timoproject/admin/service/AdminService.java
@@ -1,14 +1,23 @@
 package com.tools.seoultech.timoproject.admin.service;
 
+import com.tools.seoultech.timoproject.member.domain.Member;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import com.tools.seoultech.timoproject.member.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminService {
 
-    private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PASSWORD = "1234";
+    private static final Logger log = LoggerFactory.getLogger(AdminService.class);
+    private final MemberRepository memberRepository;
 
     /**
      * 아이디와 비밀번호를 검증하는 메서드
@@ -18,6 +27,17 @@ public class AdminService {
      * @return 인증 성공 여부
      */
     public boolean authenticate(String username, String password) {
-        return ADMIN_USERNAME.equals(username) && ADMIN_PASSWORD.equals(password);
+        Optional<Member> optionalMember = memberRepository.findByUsername(username);
+
+        // 사용자 존재 여부 및 ROLE_ADMIN 확인
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            if ("ADMIN".equals(member.getRole().name()) && ADMIN_PASSWORD.equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
+
