a7bd3d63c8733ab30f7601ff37ab5279bb5529b3
changha <mentenseoul@gmail.com>
2025-03-05 19:57:43 +0900

    [HotFix] Cors Bean

--- src/main/java/com/tools/seoultech/timoproject/auth/config/SecurityConfig.java
+++ src/main/java/com/tools/seoultech/timoproject/auth/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.tools.seoultech.timoproject.auth.jwt.filter.JwtAccessDeniedHandler;
 import com.tools.seoultech.timoproject.auth.jwt.filter.JwtAuthenticationFilter;
 import com.tools.seoultech.timoproject.auth.jwt.filter.JwtExceptionFilter;
 import com.tools.seoultech.timoproject.auth.service.CustomUserDetailsService;
+import com.tools.seoultech.timoproject.global.config.CorsConfig;
 import com.tools.seoultech.timoproject.global.config.WhitelistProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 // 새로 추가: 예외/권한 필터 관련
 
@@ -63,6 +67,7 @@ public class SecurityConfig {
 
         http
                 // ③ 필터 등록: JWT 필터 → Exception 필터
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
 
@@ -97,4 +102,26 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    /**
+     *  CORS 설정 정의 : 웹 애플리케이션의 리소스가 다른 도메인에서 안전하게 접근 허용
+     *  1. 모든 출처, 헤더, 메서드를 허용
+     *  2. 자격 증명 허용 (예: 쿠키, 인증 헤더 등)
+     *
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
