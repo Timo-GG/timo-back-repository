e1784b221133eb0112dfefa83d79f8ba8bf0f81a
changha <mentenseoul@gmail.com>
2025-03-05 20:02:23 +0900

    [HotFix] Cors Bean

--- src/main/java/com/tools/seoultech/timoproject/auth/config/SecurityConfig.java
+++ src/main/java/com/tools/seoultech/timoproject/auth/config/SecurityConfig.java
@@ -113,7 +113,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
