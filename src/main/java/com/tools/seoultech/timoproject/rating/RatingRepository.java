dbf10e808d03d6b2d126524b771c3fb2045846a9
changha <mentenseoul@gmail.com>
2025-03-11 16:25:17 +0900

    [Feat] Rating 평점 매기기 조건들 추가, 내 id 식별 추가

--- src/main/java/com/tools/seoultech/timoproject/rating/RatingRepository.java
+++ src/main/java/com/tools/seoultech/timoproject/rating/RatingRepository.java
@@ -3,9 +3,11 @@ package com.tools.seoultech.timoproject.rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findByMemberId(Long memberId);
     long countByMemberId(Long memberId);
 
+    Optional<Rating> findByMemberIdAndDuoIdAndMatchId(Long memberId, Long duoId, String matchId);
 }
