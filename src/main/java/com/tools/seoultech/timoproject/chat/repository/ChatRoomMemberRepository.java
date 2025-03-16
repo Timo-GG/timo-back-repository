9709afe5ca33f86e95b170fcd8b0ddf0c86b4401
changha <mentenseoul@gmail.com>
2025-03-16 16:45:36 +0900

    [Refactor] Rating - getDuoList() N+1 문제

--- src/main/java/com/tools/seoultech/timoproject/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/com/tools/seoultech/timoproject/chat/repository/ChatRoomMemberRepository.java
@@ -2,7 +2,9 @@ package com.tools.seoultech.timoproject.chat.repository;
 
 import com.tools.seoultech.timoproject.chat.domain.ChatRoom;
 import com.tools.seoultech.timoproject.chat.domain.ChatRoomMember;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
@@ -15,7 +17,11 @@ public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, 
 
     Collection<ChatRoomMember> findByChatRoomId(Long roomId);
 
-    List<ChatRoomMember> findByMemberId(Long memberId);
+    @Query("SELECT crm FROM ChatRoomMember crm " +
+            "JOIN FETCH crm.chatRoom cr " +
+            "JOIN FETCH crm.member m " + // Member 정보도 함께 조회하여 N+1 방지
+            "WHERE cr.id IN (SELECT crm2.chatRoom.id FROM ChatRoomMember crm2 WHERE crm2.member.id = :memberId)")
+    List<ChatRoomMember> findByMemberIdWithDuo(@Param("memberId") Long memberId);
 
     // 현재 활성화된(참여중이고 종료되지 않은) 채팅방 조회
     Optional<ChatRoomMember> findFirstByMember_IdAndIsLeftFalse(Long memberId);
