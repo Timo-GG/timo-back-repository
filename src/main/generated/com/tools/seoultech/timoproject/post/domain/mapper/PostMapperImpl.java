cf3d2a7b4a756801c2f13a66ea2986b22e34aa8d
changha <mentenseoul@gmail.com>
2025-04-13 20:13:33 +0900

    [Feat] Chat 연결 로직

--- src/main/generated/com/tools/seoultech/timoproject/post/domain/mapper/PostMapperImpl.java
+++ src/main/generated/com/tools/seoultech/timoproject/post/domain/mapper/PostMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-10T14:12:50+0900",
+    date = "2025-04-12T20:34:26+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Eclipse Adoptium)"
 )
 @Component
