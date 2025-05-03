48406bbb8fdcf3e361a4bbe812013731b1a11886
woogie01 <kwonhu3973@gmail.com>
2025-05-03 16:17:46 +0900

    [Feat] Security 경로 추가

--- src/main/java/com/tools/seoultech/timoproject/auth/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/tools/seoultech/timoproject/auth/jwt/filter/JwtAuthenticationFilter.java
@@ -54,7 +54,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             new AntPathRequestMatcher("/swagger-ui/**"),
             new AntPathRequestMatcher("/swagger-ui.html"),
             new AntPathRequestMatcher("/api/v1/univ/**"),
-            new AntPathRequestMatcher("/swagger-ui.html")
+            new AntPathRequestMatcher("/swagger-ui.html"),
+            new AntPathRequestMatcher("/api/v1/duo/**")
 
     );
 
