2c7c4e639607c8ef3cf43b5c04e448d9226ca334
changha <mentenseoul@gmail.com>
2025-05-06 15:14:18 +0900

    [Feat] Ranking 생성 시 Notification 로직

--- src/main/java/com/tools/seoultech/timoproject/ranking/facade/RankingFacadeImpl.java
+++ src/main/java/com/tools/seoultech/timoproject/ranking/facade/RankingFacadeImpl.java
@@ -1,5 +1,8 @@
 package com.tools.seoultech.timoproject.ranking.facade;
 
+import com.tools.seoultech.timoproject.notification.NotificationRequest;
+import com.tools.seoultech.timoproject.notification.NotificationService;
+import com.tools.seoultech.timoproject.notification.NotificationType;
 import com.tools.seoultech.timoproject.riot.dto.RiotRankingDto;
 import com.tools.seoultech.timoproject.riot.facade.RiotFacade;
 import com.tools.seoultech.timoproject.ranking.dto.RankingUpdateRequestDto;
@@ -18,12 +21,15 @@ import java.util.List;
 public class RankingFacadeImpl implements RankingFacade {
     private final RankingService rankingService;
     private final RankingRedisService rankingRedisService;
+    private final NotificationService notificationService;
     private final RiotFacade riotFacade;
     
     @Override
     public void createRanking(Long memberId, String puuid) {
         RiotRankingDto riotRanking = riotFacade.getRiotRanking(puuid);
         rankingRedisService.createInitialRanking(memberId, riotRanking);
+        notificationService.sendNotification(memberId,
+            new NotificationRequest(NotificationType.RANKING_REGISTERED, "/ranking"));
     }
 
     @Override
