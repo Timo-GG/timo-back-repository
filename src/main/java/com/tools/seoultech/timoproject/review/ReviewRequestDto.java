c819140800a13aa2c8b96e0bc7ea1fe2801bb988
changha <mentenseoul@gmail.com>
2025-05-09 12:40:30 +0900

    [Feat] 리뷰 도메인 작성

---
+++ src/main/java/com/tools/seoultech/timoproject/review/ReviewRequestDto.java
@@ -0,0 +1,27 @@
+package com.tools.seoultech.timoproject.review;
+
+import com.tools.seoultech.timoproject.matching.domain.myPage.entity.EnumType.OpponentAttitude;
+import com.tools.seoultech.timoproject.matching.domain.myPage.entity.EnumType.OpponentConversation;
+import com.tools.seoultech.timoproject.matching.domain.myPage.entity.EnumType.OpponentTalent;
+import com.tools.seoultech.timoproject.memberAccount.domain.entity.MemberAccount;
+
+public record ReviewRequestDto(
+        Long revieweeId,
+        OpponentAttitude attitudeScore,
+        OpponentConversation conversationScore,
+        OpponentTalent talentScore,
+        Integer evaluationScore,
+        String memo
+) {
+    public Review toEntity(MemberAccount reviewer, MemberAccount reviewee) {
+        return Review.builder()
+                .reviewer(reviewer)
+                .reviewee(reviewee)
+                .attitude_score(attitudeScore)
+                .conversation_score(conversationScore)
+                .talent_score(talentScore)
+                .evaluation_score(evaluationScore)
+                .memo(memo)
+                .build();
+    }
+}
