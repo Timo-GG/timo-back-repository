1748b5b8a4482e3fba78a8d327d3e180e5dfa84a
changha <mentenseoul@gmail.com>
2025-05-09 12:41:08 +0900

    [Refactor] 스웨거 추가

--- src/main/java/com/tools/seoultech/timoproject/global/config/SwaggerConfig.java
+++ src/main/java/com/tools/seoultech/timoproject/global/config/SwaggerConfig.java
@@ -60,6 +60,7 @@ public class SwaggerConfig {
                 "com.tools.seoultech.timoproject.auth",
                 "com.tools.seoultech.timoproject.riot",
                 "com.tools.seoultech.timoproject.notification",
+                "com.tools.seoultech.timoproject.review",
 
         };
         return GroupedOpenApi.builder()
