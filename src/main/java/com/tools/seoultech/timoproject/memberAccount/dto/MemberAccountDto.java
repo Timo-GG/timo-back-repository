9bdeaaa167f6680e2191e0cc5238d94d3563da90
woogie01 <kwonhu3973@gmail.com>
2025-05-10 19:45:03 +0900

    [Fix] 소환사 아이콘 업데이트 안되는 버그 수정

--- src/main/java/com/tools/seoultech/timoproject/memberAccount/dto/MemberAccountDto.java
+++ src/main/java/com/tools/seoultech/timoproject/memberAccount/dto/MemberAccountDto.java
@@ -27,13 +27,15 @@
         public record RiotAccountDto(
                 String puuid,
                 String accountName,
-                String accountTag
+                String accountTag,
+                String profileUrl
         ) {
             public static RiotAccountDto from(RiotAccount entity) {
                 return new RiotAccountDto(
                         entity.getPuuid(),
                         entity.getAccountName(),
-                        entity.getAccountTag()
+                        entity.getAccountTag(),
+                        entity.getProfileUrl()
                 );
             }
         }
