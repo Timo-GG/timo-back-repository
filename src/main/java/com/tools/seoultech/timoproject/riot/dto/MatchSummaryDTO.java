d05bad8cac1cd28e603241158683019ef398cf66
changha <mentenseoul@gmail.com>
2025-07-08 15:24:03 +0900

    [Chore] @Slf4j 추가

--- src/main/java/com/tools/seoultech/timoproject/riot/dto/MatchSummaryDTO.java
+++ src/main/java/com/tools/seoultech/timoproject/riot/dto/MatchSummaryDTO.java
@@ -1,10 +1,5 @@
 package com.tools.seoultech.timoproject.riot.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 public record MatchSummaryDTO(
